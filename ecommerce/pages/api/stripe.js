1ecc824dccfee021a1f34b30b56d9b8b832e5027
nikballal <nikhil.ballal1994@gmail.com>
2022-09-02 14:27:57 +1000

    stripe API integration

---
+++ ecommerce/pages/api/stripe.js
@@ -0,0 +1,57 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body);
+
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1LdP0WFmgIBDvbbXZJdrs7Wi" },
+          { shipping_rate: "shr_1LdRKeFmgIBDvbbXVCyTWtf5" },
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/vub4zvvx/production/"
+            )
+            .replace("-webp", ".webp"); //get project id from sanity desk
+
+          return {
+            price_data: {
+              currency: "aud",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?cancelled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
